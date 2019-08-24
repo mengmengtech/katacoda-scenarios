6eb205a41c19bdf49fd926a41e1a69a7d4a68b08
wanghw <wanghw@sh-stt.com>
2019-08-24 23:22:40 +0800

    update

--- upload-assets/assets/deploy.sh
+++ upload-assets/assets/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd /root/
-mkdir /root/ai
-mkdir /root/ai/ml1-1
-mkdir /root/ai/ml1-1/c1
-mkdir /root/ai/ml1-1/c2
-mkdir /root/ai/ml1-1/c3
+git clone https://github.com/mengmengtech/katacoda-scenarios.git
+pip3 install matplotlib
+pip3 install sklearn
+# mkdir /root/ai
+# mkdir /root/ai/ml1-1
+# mkdir /root/ai/ml1-1/c1
+# mkdir /root/ai/ml1-1/c2
+# mkdir /root/ai/ml1-1/c3
 
-touch hello-world
+# touch hello-world
