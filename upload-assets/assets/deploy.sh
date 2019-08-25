ff5493221ffeb2a9c44163a2a61285ff86f05ea9
wanghw <wanghw@sh-stt.com>
2019-08-25 21:01:54 +0800

    update

--- upload-assets/assets/deploy.sh
+++ upload-assets/assets/deploy.sh
@@ -3,10 +3,15 @@ cd /root/
 git clone https://github.com/mengmengtech/katacoda-scenarios.git
 pip3 install matplotlib
 pip3 install sklearn
-# mkdir /root/ai
+mkdir /root/ai
 # mkdir /root/ai/ml1-1
 # mkdir /root/ai/ml1-1/c1
 # mkdir /root/ai/ml1-1/c2
 # mkdir /root/ai/ml1-1/c3
 
 # touch hello-world
+
+docker pull jupyter/datascience-notebook
+docker run -itd -p 80:8888 -v /root/ai:/root/ai --name="mmjupyter" jupyter/datascience-notebook bash
+docker exec -it mmjupyter bash
+jupyter notebook &
