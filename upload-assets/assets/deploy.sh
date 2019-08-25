91434509c667138abac6d19f008991e364a19438
wanghw <wanghw@sh-stt.com>
2019-08-25 21:46:16 +0800

    update

--- upload-assets/assets/deploy.sh
+++ upload-assets/assets/deploy.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
+/home/jovyan
 cd /opt/
+mkdir /opt/ai
+cd /opt/ai
 git clone https://github.com/mengmengtech/katacoda-scenarios.git
 git clone https://github.com/jakevdp/PythonDataScienceHandbook.git
 git clone https://github.com/jakevdp/sklearn_tutorial.git
 pip3 install matplotlib
 pip3 install sklearn
-mkdir /opt/ai
-# mkdir /root/ai/ml1-1
-# mkdir /root/ai/ml1-1/c1
-# mkdir /root/ai/ml1-1/c2
-# mkdir /root/ai/ml1-1/c3
-
-# touch hello-world
 
 docker pull jupyter/datascience-notebook
-docker run -itd -p 80:8888 -v /opt/ai:/opt/ai --name="mmjupyter" jupyter/datascience-notebook bash
+docker run -itd -p 80:8888 -v /home/jovyan/work:/opt/ai --name="mmjupyter" jupyter/datascience-notebook bash
 docker exec -it mmjupyter bash
-cd /opt/ai/
 jupyter notebook &
